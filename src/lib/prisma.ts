9e168cdb7110e6245565336bd64492be573a371c
Matt Allen <allen.matt@gmail.com>
2025-12-04 07:23:58 -0500

    setup prisma and supabase

---
+++ src/lib/prisma.ts
@@ -0,0 +1,29 @@
+/**
+ * Prisma Client Singleton
+ *
+ * This pattern prevents multiple Prisma Client instances in development
+ * due to Next.js hot reloading. In production, a single instance is used.
+ *
+ * @see https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+ */
+
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log:
+			process.env.NODE_ENV === 'development'
+				? ['query', 'error', 'warn']
+				: ['error'],
+	});
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
