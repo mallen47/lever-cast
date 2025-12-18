1fb956dffe190172287f2729e78f41ddfd416362
Matt Allen <allen.matt@gmail.com>
2025-12-18 07:01:52 -0500

    add caching for recent posts

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -1,11 +1,8 @@
 export { generateFormattedContent } from './content-generator';
-export {
-	getTemplates,
-	getTemplateById,
-	getTemplatesByCategory,
-} from './templates';
+export { getTemplates, getTemplateById } from './templates';
 export {
 	fetchPosts,
+	POSTS_SWR_KEY,
 	fetchPostsByStatus,
 	fetchPostById,
 	searchPostsByQuery,
